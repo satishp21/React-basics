437f335c64b8fabe0a4e3104c8c6372fbd1c3dbf
satishp21 <satishdpanchal786@gmail.com>
2023-06-28 16:41:41 +0530

    main component and sub component created

---
+++ src/components/Expenseitem.js
@@ -0,0 +1,9 @@
+function ExpenseItem(){
+    return <ul>Expense items
+        <li>Food rs 100</li>
+        <li>petrol rs 500</li>
+        <li>movie rs 570</li>
+    </ul>
+}
+
+export default ExpenseItem
