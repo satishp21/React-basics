60497654e6d5d287240f862bc182c66c806379a0
satishp21 <satishdpanchal786@gmail.com>
2023-06-29 22:07:30 +0530

    added a date component inside expense item comp

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,12 +1,12 @@
+import ExpenseDate from './ExpenseDate'
 import './Expenseitem.css'
 
 function ExpenseItem(props){
-
     //always remember that return only return one main eliment that why in given below html is wrapped inside div
     // () thia also shows that single html eliment to be returned
     return (
         <div className='expense-item'>
-            <div>{props.date.toISOString()}</div>
+            <ExpenseDate date = {props.date} />
             <h2>{props.date.location}</h2>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
