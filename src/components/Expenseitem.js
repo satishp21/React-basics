7f77af06321b203b4274455ed71a418c86950b59
satishp21 <satishdpanchal786@gmail.com>
2023-06-29 20:01:28 +0530

    dynamically passing data in components from props

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,21 +1,17 @@
 import './Expenseitem.css'
 
-function ExpenseItem(){
-    const expenseDate = new Date(2023,29,6);
-    const expenseTitle = 'Food';
-    const expenseAmount = 100;
-    const expenseLocation = 'restorent';
+function ExpenseItem(props){
 
     //always remember that return only return one main eliment that why in given below html is wrapped inside div
     // () thia also shows that single html eliment to be returned
     return (
         <div className='expense-item'>
-            <div>{expenseDate.toISOString()}</div>
-            <h2>{expenseLocation}</h2>
+            <div>{props.date.toISOString()}</div>
+            <h2>{props.date.location}</h2>
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>
-                <h2>{expenseAmount}</h2>
+                <h2>{props.amount}</h2>
                 </div>
             </div>
         </div>)
